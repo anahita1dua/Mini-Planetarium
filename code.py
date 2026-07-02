4647afb34434b30a919049d30627c3ca20f14c45
anahita1dua <anahita1dua08@gmail.com>
2026-07-02 15:52:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,11 @@
 import customtkinter as ctk
-from 
+from geopy.geocoders import Nominatim #used for finding coordinates
+from skyfield.api import Topos, load
+from skyfield.api import hipparcos 
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+class MiniPlanetarium:
